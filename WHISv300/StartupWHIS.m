e034c142584732499e16875b236e2fe83a226be2
irino <irino@wakayama-u.ac.jp>
2022-01-26 13:05:40 +0900

    Function StartupWHIS.m

--- WHISv300/StartupWHIS.m
+++ WHISv300/StartupWHIS.m
@@ -5,10 +5,13 @@
 %      Modified:   14 Sep 21
 %      Modified:   26 Sep 21
 %      Modified:    1  Dec  21
+%      Modified:   22 Jan 2022  % defined as a function not to overwrite  DirProg
 %
 %      Setting path at least once before starting WHIS programs
 %   
-DirProg = fileparts(which(mfilename));
+function DirProg = StartupWHIS
+
+DirProg = fileparts(which(mfilename)); % Directory of this program
 addpath([DirProg '/Tool/']);
 addpath([DirProg '/Tool/OneThirdOctFB/']);    
 
@@ -18,3 +21,4 @@ NameGCFB = 'GCFBv231';
 addpath([DirProg '/../../' DirGCFB '/' NameGCFB '/']);    
 addpath([DirProg '/../../' DirGCFB '/' NameGCFB '/Tool/']);   
 
+end
